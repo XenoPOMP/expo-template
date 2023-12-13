af0622c3347be1a268423800ea2c7d9266469089
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-13 23:57:53 +0300

    import aliases not working for now

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HelloWorld from '@/components/HelloWorld';
+import HelloWorld from './app/components/HelloWorld';
 
 export default function App() {
   return (
