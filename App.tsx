49ae7f5e197cab025e7f8adbfa1dd63095c74a42
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-14 00:54:18 +0300

    install react-query

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Application } from '@/layout';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Application />
+      <QueryClientProvider client={queryClient}>
+        <Application />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
