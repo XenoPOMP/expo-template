8978a7404c39c39933a6428484e48628923f1b9c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-13 22:38:51 +0300

    add project files to TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import { type Config } from 'tailwindcss';
 // import ThemerPlugin from 'tailwindcss-themer';
 
 const twConfig: Config = {
-  content: [],
+  content: ['./App.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
