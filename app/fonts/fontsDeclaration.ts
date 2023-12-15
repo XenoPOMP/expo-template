cfbf7e51fefb9cfdecb8a78c7d9551c537c202be
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-15 20:21:43 +0300

    install SF Pro Display font

---
+++ app/fonts/fontsDeclaration.ts
@@ -0,0 +1,49 @@
+type WithItalic<T extends string> = T | `${T}Italic`;
+
+type FontDeclaration = Record<
+  string,
+  Partial<
+    | Record<
+        | WithItalic<'Ultralight'>
+        | WithItalic<'Thin'>
+        | WithItalic<'Light'>
+        | WithItalic<'Regular'>
+        | WithItalic<'Medium'>
+        | WithItalic<'Semibold'>
+        | WithItalic<'Bold'>
+        | WithItalic<'Heavy'>
+        | WithItalic<'Black'>,
+        string
+      >
+    | string
+  >
+>;
+
+/**
+ * Get names of each style for each font in project.
+ *
+ * @example
+ *  <Text
+ *   className={'text-white'}
+ *   style={{
+ *     fontFamily: fontsDeclaration.Kalnia,
+ *     fontSize: 30,
+ *   }}
+ * >
+ *   Kalnia font
+ * </Text>
+ */
+const fontsDeclaration = {
+  Kalnia: 'Kalnia',
+
+  SFProDisplay: {
+    Regular: 'SF-Pro-Display-Regular',
+    RegularItalic: 'SF-Pro-Display-Regular-Italic',
+    Medium: 'SF-Pro-Display-Medium',
+    MediumItalic: 'SF-Pro-Display-Medium-Italic',
+    Bold: 'SF-Pro-Display-Bold',
+    BoldItalic: 'SF-Pro-Display-Bold-Italic',
+  },
+} satisfies FontDeclaration;
+
+export default fontsDeclaration;
--- app/layout/application/Application.tsx
+++ app/layout/application/Application.tsx
@@ -1,15 +1,45 @@
 import cn from 'classnames';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import fontsDeclaration from '@/fonts/fontsDeclaration';
 import { flexCenter } from '@/styles/tailwind/utilities';
+
+SplashScreen.preventAutoHideAsync();
 
 const Application = () => {
   const { top, bottom, left, right } = useSafeAreaInsets();
 
+  const [fontsLoaded, fontError] = useFonts({
+    Kalnia: require('../../fonts/google fonts/Kalnia.ttf'),
+
+    'SF-Pro-Display-Regular': require('@/fonts/SFProDisplay/SF-Pro-Display-Regular.otf'),
+    'SF-Pro-Display-Regular-Italic': require('@/fonts/SFProDisplay/SF-Pro-Display-RegularItalic.otf'),
+
+    'SF-Pro-Display-Medium': require('@/fonts/SFProDisplay/SF-Pro-Display-Medium.otf'),
+    'SF-Pro-Display-Medium-Italic': require('@/fonts/SFProDisplay/SF-Pro-Display-MediumItalic.otf'),
+
+    'SF-Pro-Display-Bold': require('@/fonts/SFProDisplay/SF-Pro-Display-Bold.otf'),
+    'SF-Pro-Display-Bold-Italic': require('@/fonts/SFProDisplay/SF-Pro-Display-BoldItalic.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View
+      onLayout={onLayoutRootView}
       className={cn('bg-purple-950 h-full', flexCenter)}
       style={{
         paddingTop: top,
@@ -18,9 +48,34 @@ const Application = () => {
         paddingRight: right,
       }}
     >
-      <Text className={'text-2xl text-white'}>Hello world!</Text>
+      <Text
+        className={'text-white'}
+        style={{
+          fontSize: 30,
+        }}
+      >
+        System font
+      </Text>
 
-      <Text className={'text-2xl text-white'}>Kalnia font</Text>
+      <Text
+        className={'text-white'}
+        style={{
+          fontFamily: fontsDeclaration.Kalnia,
+          fontSize: 30,
+        }}
+      >
+        Kalnia font
+      </Text>
+
+      <Text
+        className={'text-white'}
+        style={{
+          fontFamily: fontsDeclaration.SFProDisplay.Bold,
+          fontSize: 30,
+        }}
+      >
+        SF Pro Display font (Bold)
+      </Text>
 
       <StatusBar style='auto' />
     </View>
